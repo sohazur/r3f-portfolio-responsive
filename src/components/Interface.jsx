0a29997eb36578c2faa62dd4a1d937323aa7663a
Sohazur <mdsohazurislam@gmail.com>
2025-01-04 06:58:09 +0400

    updated projects

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -251,9 +251,7 @@ const ProjectsSection = () => {
         >
           ← Previous
         </button>
-        <h2 className="text-3xl md:text-5xl font-bold">
-          Achievements & Projects
-        </h2>
+        <h2 className="text-3xl md:text-5xl font-bold">Projects</h2>
         <button
           className="hover:text-indigo-600 transition-colors"
           onClick={nextProject}
